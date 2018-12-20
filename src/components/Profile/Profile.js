cf6ce324351701b79f949c7928dd2b2c7562491b
Gino Capio <ginocapio@gmail.com>
2018-12-19 18:56:10 -0600

    Add idea to gitignore and use props for profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Profile extends Component {
-    render() {
-        return (
-            <div className="card">
-                <img className="card-img-top" src="https://randomuser.me/api/portraits/lego/8.jpg" alt="Card cap" />
-                <div className="card-body">
-                    <p className="card-text">
-                        <b>@ilovelove</b>
-                        <hr />
-                        <span>I like that energy! BOP!</span>
-                    </p>
-                </div>
-            </div>
-        );
-    }
+
+  render() {
+    return (
+        <div className="card">
+          <img className="card-img-top"
+               src={this.props.profilePicture}
+               alt="Card cap"/>
+          <div className="card-body">
+            <p className="card-text">
+              <b>{this.props.username}</b>
+              <hr/>
+              <span>{this.props.bio}</span>
+            </p>
+          </div>
+        </div>
+    );
+  }
 }
 
-export default Profile
+export default Profile;
